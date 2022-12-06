9f74fa4ad0e37cb272b9e5a64075547880f7a700
Thinh Nguyen <thinh@datajoint.com>
2022-12-06 15:32:25 -0600

    session 6

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -23,7 +23,7 @@ standard_worker = DataJointWorker('standard_worker',
                                   autoclear_error_patterns=autoclear_error_patterns)
 
 # restrict to 1 session
-analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 5}
+analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 6}
 
 standard_worker(analysis_pop.ROISVDPython, max_calls=20)
 
