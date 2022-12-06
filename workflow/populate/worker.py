e771165e59b6aeb0773322162514b0b23c80f50e
Thinh Nguyen <thinh@datajoint.com>
2022-12-06 15:32:17 -0600

    add examples for new datajoint tables for analysis

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -3,6 +3,7 @@ from datajoint_utilities.dj_worker import DataJointWorker, WorkerLog, ErrorLog
 
 from workflow import db_prefix
 from workflow.pipeline import analysis_pop
+# from workflow.pipeline import analysis_new   # import another schema in the future
 
 logger = dj.logger
 
@@ -25,4 +26,17 @@ standard_worker = DataJointWorker('standard_worker',
 analysis_pop.ROISVDPython.key_source &= {'subject_id': '464724', 'session': 5}
 
 standard_worker(analysis_pop.ROISVDPython, max_calls=20)
-# standard_worker(analysis_pop.FutureNewAnalysisTable, max_calls=20)
+
+# example to add new workers  
+
+# arseny_worker = DataJointWorker('arseny_worker',
+#                                   worker_schema_name,
+#                                   db_prefix=[db_prefix],
+#                                   run_duration=1,
+#                                   sleep_duration=20,
+#                                   autoclear_error_patterns=autoclear_error_patterns)
+
+# arseny_worker(analysis_new.FutureNewAnalysisTable, max_calls=20)
+# arseny_worker(analysis_new.TableAnalysis, max_calls=20)
+
+# 
